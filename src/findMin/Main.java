e94a8ef07216d76a31ab18fbcab9eb2c66acf043
nmtuyen <nguyenmanhtuyen.haui@gmail.com>
2021-05-30 23:05:33 +0700

    find min

---
+++ src/findMin/Main.java
@@ -0,0 +1,23 @@
+package findMin;
+
+import java.util.List;
+
+public class Main {
+    public static int findMin(List<Integer> nums){
+        int min = nums.get(0);
+        for (int i = 0; i < nums.size(); i++){
+            if (min < nums.get(i)){
+                min = nums.get(i);
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        ReadWriteFile readWriteFile = new ReadWriteFile();
+
+        List<Integer> nums = readWriteFile.readFile("numbers.txt");
+        int minValue = findMin(nums);
+        readWriteFile.writeFile("ketqua.txt", minValue);
+    }
+}
---
+++ src/findMin/ReadWriteFile.java
@@ -0,0 +1,34 @@
+package findMin;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadWriteFile {
+    List<Integer> readFile(String filePath){
+        List<Integer> nums = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String lineValue;
+            while ((lineValue = bufferedReader.readLine())!= null){
+                nums.add(Integer.parseInt(lineValue));
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File không tồn tại or file lỗi");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nums;
+    }
+    void writeFile(String filePath, int min){
+        try {
+            FileWriter fileWriter = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Giá trị nhỏ nhất là: "+ min);
+        } catch (IOException e) {
+            System.err.println("File không tồn tại or file lỗi");
+        }
+    }
+}
